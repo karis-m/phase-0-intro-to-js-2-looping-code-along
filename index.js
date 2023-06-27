f235212f9b71baa72e53e3d7b4c5719dad80e4b8
moses <moses.kamau@student.moringaschool.com>
2023-06-28 00:04:06 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+
+function writeCards(names = ["Guadalupe", "Ollie", "Aki"], event = "surprise") {
+
+    for (let i = 0; i < names.length; i++) {
+        let myArray = new Array("Thank you, " + names[0] + ", for the wonderful " + event + " gift!",
+            "Thank you, " + names[1] + ", for the wonderful " + event + " gift!",
+            "Thank you, " + names[2] + ", for the wonderful " + event + " gift!");
+        return myArray;
+    }
+}
+
+function countDown(num =10){
+    let i=0;
+    while (i <= num){
+    console.log(num--);
+    }
+}
